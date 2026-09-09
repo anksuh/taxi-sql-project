31827b33a4ab4963597782f3ba3cef72d4f543ae
Ankush Singh <your-github-email@example.com>
2026-09-09 00:10:49 -0400

    feat(v1): create raw_trips staging table schema

---
+++ sql/01_raw_staging.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS raw_trips (
+    vendorid                INT,
+    tpep_pickup_datetime     TIMESTAMP,
+    tpep_dropoff_datetime    TIMESTAMP,
+    passenger_count          NUMERIC,
+    trip_distance            NUMERIC,
+    ratecodeid               NUMERIC,
+    store_and_fwd_flag       TEXT,
+    pulocationid             INT,
+    dolocationid             INT,
+    payment_type             INT,
+    fare_amount              NUMERIC,
+    extra                    NUMERIC,
+    mta_tax                  NUMERIC,
+    tip_amount               NUMERIC,
+    tolls_amount             NUMERIC,
+    improvement_surcharge    NUMERIC,
+    total_amount             NUMERIC,
+    congestion_surcharge     NUMERIC
+);
